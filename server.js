264a4197acf5e100b94e716d8b06e07236e183a1
yamkarki99 <99rajeevkarki@gmail.com>
2023-06-11 14:18:17 +1000

    firstcommit

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+const app = express();
+
+import cors from "cors";
+
+const PORT = 8000;
+
+// connect mongodb
+import { mongoConnect } from "./src/config/mongoDb.js";
+mongoConnect();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// API endpoints
+
+import taskRouter from "./src/router/taskRouter.js";
+
+app.use("/api/v1/task", taskRouter);
+
+app.use("/", (req, res) => {
+  res.json({ message: "server running healthy" });
+});
+
+// open port for http request to access the server
+app.listen(PORT, (err) => {
+  err
+    ? console.log(err.message)
+    : console.log(`Server running at http://localhost:${PORT}`);
+});
---
+++ src/router/taskRouter.js
@@ -0,0 +1,90 @@
+import express from "express";
+import {
+  createTask,
+  deleteTaskById,
+  readTasks,
+  switchTask,
+} from "../model/TaskModel.js";
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  //get data from the db
+  const taskList = await readTasks();
+
+  res.json({
+    status: "success",
+    message: "From Get method",
+    taskList,
+  });
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const result = await createTask(req.body);
+
+    result?._id
+      ? res.json({
+          status: "success",
+          message: "New task has been added successfully",
+        })
+      : res.json({
+          status: "success",
+          message: "unable to add the data",
+        });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.patch("/", async (req, res) => {
+  try {
+    const { _id, type } = req.body;
+    // update data in db
+    const result = await switchTask(_id, type);
+
+    result?._id
+      ? res.json({
+          status: "success",
+          message: "The task has been switeched successfully",
+        })
+      : res.json({
+          status: "error",
+          message: "The task did not switeched ",
+        });
+  } catch (error) {
+    console.log(error);
+
+    res.json({
+      status: "error",
+      message: "The task did not switeched ",
+    });
+  }
+});
+
+router.delete("/:_id", async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const result = await deleteTaskById(_id);
+
+    console.log(result);
+
+    result?._id
+      ? res.json({
+          status: "success",
+          message: "The task has been deleted successfully",
+        })
+      : res.json({
+          status: "error",
+          message: "Unable to delete the task ",
+        });
+  } catch (error) {
+    console.log(error);
+
+    res.json({
+      status: "error",
+      message: "Error deleting the task",
+    });
+  }
+});
+
+export default router;
